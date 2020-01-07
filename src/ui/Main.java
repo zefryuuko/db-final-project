fb5e4c883e42df8765c1c139eae1ca3516095582
Zef <titotobing0511@gmail.com>
2020-01-07 14:52:47 +0700

    Call DBConnect constructor on program launch

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,5 +1,7 @@
 package ui;
 
+import database.DBConnect;
+import database.Staff;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -7,6 +9,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    private static DBConnect dbConnect = new DBConnect();
 
     @Override
     public void start(Stage primaryStage) throws Exception{
