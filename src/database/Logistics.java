fe55d9338ad22c56d4c70bd14aebba4eb5cd096e
Zef <titotobing0511@gmail.com>
2020-01-08 03:49:14 +0700

    Add getLogisticsDetails

--- src/database/Logistics.java
+++ src/database/Logistics.java
@@ -1,9 +1,40 @@
 package database;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Logistics extends DBConnect {
     // CREATE
 
     // READ
+    public static ArrayList<HashMap<String, String>> getLogisticsDetails() {
+        ArrayList<HashMap<String, String>> result = new ArrayList<>();
+
+        try {
+            String query = "SELECT l.logistic_id, l.staff_id, s.cust_name, l.cust_address, p.provider_name, l.tracking_number, l.date_sent FROM Logistics l LEFT JOIN Sales s ON s.sales_id = l.sales_id LEFT JOIN LogisticsProvider p ON p.provider_id = l.logistic_provider;";
+            PreparedStatement pst = connection.prepareStatement(query);
+            ResultSet rs = pst.executeQuery();
+
+            while (rs.next()) {
+                HashMap<String, String> row = new HashMap<>();
+                row.put("id", Integer.toString(rs.getInt("logistic_id")));
+                row.put("staffId", Integer.toString(rs.getInt("staff_id")));
+                row.put("custName", rs.getString("cust_name"));
+                row.put("custAddress", rs.getString("cust_address"));
+                row.put("providerName", rs.getString("provider_name"));
+                row.put("trackingNumber", Integer.toString(rs.getInt("tracking_number")));
+                row.put("dateSent", rs.getDate("date_sent").toString());
+                result.add(row);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return result;
+        }
+
+        return result;
+    }
 
     // UPDATE
 
